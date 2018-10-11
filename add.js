3ec6f81bfa5d2b93c6bf3fd3ffe0c3559df6b719
Birgir Hlynur Guðmundsson <BirgirHlynur@clients-pool-1.nat.ru.is>
2018-10-11 16:22:19 +0000

    add.js throw'ar núna exception ef það er negatív tala og summar ekki tölur sem eru stærri en 1000

--- add.js
+++ add.js
@@ -8,7 +8,7 @@
 function Add(numbers)
 {
     
-    var x = "0";
+    var x = "-5";
     var y = "1";
     var z = "2";
     
@@ -16,9 +16,39 @@ function Add(numbers)
     var num2 = parseInt(y);
     var num3 = parseInt(z);
     
+    if (num1 < 0)
+    {
+        console.log("Negatives not allowed: " + num1);
+    }
+    else if (num1 > 1000)
+    {
+        var sum = num2 + num3;
+        console.log(sum);
+    }
+    else if (num2 < 0)
+    {
+        console.log("Negatives not allowed: " + num2);
+    }
+    else if (num2 > 1000)
+    {
+        var sum = num1 + num3;
+        console.log(sum);
+    }
+    else if (num3 < 0)
+    {
+        console.log("Negatives not allowed: " + num3);
+    }
+    else if (num3 > 1000)
+    {
+        var sum = num1 + num2;
+        console.log(sum);
+    }
+    else
+    {
     var sum = num1 + num2 + num3;
-    
     console.log(sum);
+    }
+    
     
 }
 

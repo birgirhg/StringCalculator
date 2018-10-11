4a30a81026c0ac12f6941cfa2aac9331b724a723
Birgir Hlynur Guðmundsson <BirgirHlynur@clients-pool-1.nat.ru.is>
2018-10-11 15:57:35 +0000

    add.js summar nú tölur innan strengs. Það á enn eftir að skrifa kóða sem kemur í veg fyrir negatívar tölur, tölur stærri en 1000 ofl.

--- add.js
+++ add.js
@@ -7,7 +7,19 @@
 
 function Add(numbers)
 {
-    console.log("Testing 1 2 3...");
+    
+    var x = "0";
+    var y = "1";
+    var z = "2";
+    
+    var num1 = parseInt(x);
+    var num2 = parseInt(y);
+    var num3 = parseInt(z);
+    
+    var sum = num1 + num2 + num3;
+    
+    console.log(sum);
+    
 }
 
     

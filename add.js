b4941e4e992a4580b9f332043af7963c66cf84ec
Birgir Hlynur Guðmundsson <BirgirHlynur@clients-pool-1.nat.ru.is>
2018-10-11 15:32:01 +0000

    Fyrsta commit, test run

---
+++ add.js
@@ -0,0 +1,14 @@
+// function Add(numbers).js
+
+/* Skrifa test fyrir eftirfarandi:
+    1. Tölur mega ekki vera stærri en 1000
+    2. Forritið tekur ekki við negatívum tölum
+*/
+
+function Add(numbers)
+{
+    console.log("Testing 1 2 3...");
+}
+
+    
+Add();

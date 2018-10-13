526ab9d6a11eaecad57d4e783790be89b3e8e22e
Birgir Hlynur Guðmundsson <BirgirHlynur@Birgirs-MacBook-Air-3.local>
2018-10-13 20:17:41 +0000

    Kóði les nú inn streng sem er tómur en skilar hinsvegar NaN, vinn í lausn

--- add.js
+++ add.js
@@ -7,7 +7,7 @@
 
 function Add(numbers)
 {
-    var x = "0";
+    var x = "";
     var y = "1";
     var z = "2";
     
@@ -15,9 +15,51 @@ function Add(numbers)
     var num2 = parseInt(y);
     var num3 = parseInt(z);
     
+    if (num1 === "")
+    {
+        num1 === 0;
+
+    }
+    else if (num2 === "")
+    {
+        num2 === 0;
+    }  
+    else if (num3 === "")
+    {
+        num3 === 0;
+    }
+    else if (num1 < 0)
+    {
+        throw ("Negatives not allowed: " + num1);
+    }
+    else if (num1 > 1000)
+    {
+        var sum = num2 + num3;
+        console.log(sum);
+    }
+    else if (num2 < 0)
+    {
+        throw ("Negatives not allowed: " + num2);
+    }
+    else if (num2 > 1000)
+    {
+        var sum = num1 + num3;
+        console.log(sum);
+    }
+    else if (num3 < 0)
+    {
+        throw ("Negatives not allowed: " + num3);
+    }
+    else if (num3 > 1000)
+    {
+        var sum = num1 + num2;
+        console.log(sum);
+    }
+    else
+    {
     var sum = num1 + num2 + num3;
-    
     console.log(sum);
+    }
 }
 
     

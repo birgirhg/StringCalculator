28ace4c29288422ed087dc4e8189b0ab767c4a68
Birgir Hlynur Guðmundsson <BirgirHlynur@Birgirs-MacBook-Air-3.local>
2018-10-13 19:48:00 +0000

    TDD Kata trial run 2 - tóm skrá

--- add.js
+++ add.js
@@ -7,49 +7,7 @@
 
 function Add(numbers)
 {
-    
-    var x = "-5";
-    var y = "0";
-    var z = "0";
-    
-    var num1 = parseInt(x);
-    var num2 = parseInt(y);
-    var num3 = parseInt(z);
-    
-    if (num1 < 0)
-    {
-        throw ("Negatives not allowed: " + num1);
-    }
-    else if (num1 > 1000)
-    {
-        var sum = num2 + num3;
-        console.log(sum);
-    }
-    else if (num2 < 0)
-    {
-        throw ("Negatives not allowed: " + num2);
-    }
-    else if (num2 > 1000)
-    {
-        var sum = num1 + num3;
-        console.log(sum);
-    }
-    else if (num3 < 0)
-    {
-        throw ("Negatives not allowed: " + num3);
-    }
-    else if (num3 > 1000)
-    {
-        var sum = num1 + num2;
-        console.log(sum);
-    }
-    else
-    {
-    var sum = num1 + num2 + num3;
-    console.log(sum);
-    }
-    
-    
+    console.log("Hello, World!");
 }
 
     

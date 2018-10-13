dbf7deca30eaeb0d923a63f808eebd6fcc70e660
Birgir Hlynur Guðmundsson <BirgirHlynur@Birgirs-MacBook-Air-3.local>
2018-10-13 19:51:12 +0000

    Kóði tekur þrjár breytur og summar þær saman

--- add.js
+++ add.js
@@ -7,7 +7,17 @@
 
 function Add(numbers)
 {
-    console.log("Hello, World!");
+    var x = "0";
+    var y = "1";
+    var z = "2";
+    
+    var num1 = parseInt(x);
+    var num2 = parseInt(y);
+    var num3 = parseInt(z);
+    
+    var sum = num1 + num2 + num3;
+    
+    console.log(sum);
 }
 
     
